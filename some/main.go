851c9b6589787810848fa8b7bf03f9f7f3a095ac
Michele Sciabarra <michele@nuvolaris.io>
2023-02-11 13:33:22 +0000

    test command

---
+++ some/main.go
@@ -0,0 +1,28 @@
+package some
+
+import (
+	"fmt"
+
+	"github.com/nuvolaris/someutils"
+)
+
+func IsBuiltin(name string) bool {
+	switch name {
+	case "basename", "cat", "cp", "dirname",
+		"grep", "gunzip", "gzip", "head",
+		"ls", "mv", "pwd", "rm", "sleep",
+		"tail", "tar", "tee", "touch", "tr",
+		"unzip", "wc", "wget", "which", "zip":
+		return true
+	}
+	return false
+}
+
+func Run(name string, args []string) (int, error) {
+	if !IsBuiltin(name) {
+		return 1, fmt.Errorf("command %s not found", name)
+	}
+	full := append([]string{name}, args...)
+	err, code := someutils.Call(name, full)
+	return code, err
+}
